a191b8952a03cee77b74cba42fd0beafc951b04d
kazuya kawaguchi <kawakazu80@gmail.com>
2021-09-12 00:15:47 +0900

    add vue-i18n-loader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,16 @@
-export default {
-}
+import { defineNuxtConfig, extendWebpackConfig } from '@nuxt/kit'
+
+export default defineNuxtConfig({
+  modules: [{
+    setup(options, nuxt) {
+      console.log('module setup')
+      extendWebpackConfig((config => {
+	config.module.rules.push({
+	  resourceQuery: /blockType=i18n/,
+	  type: 'javascript/auto',
+	  loader: '@intlify/vue-i18n-loader'
+	})
+      }))
+    }
+  }]
+})
--- pages/fetching/count.vue
+++ pages/fetching/count.vue
@@ -1,4 +1,8 @@
 <script setup nuxt>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n({
+  inheritLocale: true
+})
 const ts = new Date().getTime()
 const { data } = asyncData('k1', () => $fetch('/api/count'))
 console.log('call /api/count', data)
@@ -6,7 +10,19 @@ console.log('call /api/count', data)
 
 <template>
   <div>
+    <p>{{ t('fetching', { count: data }) }}</p>
     <p>fetching count: {{ data }}</p>
     <NuxtLink to="/fetching/">back to /fetching/</NuxtLink>
   </div>
 </template>
+
+<i18n lang="json">
+{
+  "en": {
+    "fetching": "fetching count: {count}"
+  },
+  "ja": {
+    "fetching": "フェッチカウント: {count}"
+  }
+}
+</i18n>
